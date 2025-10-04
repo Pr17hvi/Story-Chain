7079711434db2000440a39b1655efabd27a16de7
Prithvi Singh <prithvi7091@gmail.com>
2025-10-04 20:30:20 +0530

    again

--- storychain/server/render-build.sh
+++ storychain/server/render-build.sh
@@ -1,7 +1,14 @@
-#!/usr/bin/env bash 
-set -o errexit 
-npm install 
-cd ../client 
-npm install 
-npm run build 
-cd ../server 
+#!/usr/bin/env bash
+set -o errexit
+
+# install server deps
+npm install
+
+# move to client
+cd ../client
+# install client deps including devDependencies
+npm install --include=dev
+npm run build
+
+# back to server
+cd ../server
